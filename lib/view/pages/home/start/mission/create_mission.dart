fe8717a35864143b8fac1a22961cb46f0701a3a2
amine-zergan <amine.mejri1000@gmail.com>
2023-05-09 19:05:56 +0100

    mission view completed

--- lib/view/pages/home/start/mission/create_mission.dart
+++ lib/view/pages/home/start/mission/create_mission.dart
@@ -1,11 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:telecom/db/helpers/constant_db.dart';
+
 import 'package:telecom/view/components/Form_Fields/nom_form_field.dart';
 import 'package:telecom/view/pages/home/start/components/intro_title_page.dart';
 import 'package:telecom/view/pages/home/start/site/components/title_component.dart';
 import 'package:telecom/view/pages/home/start/site/components/title_section_component.dart';
 import 'package:telecom/view/theme/size_constants.dart';
+
 import '../components/app_bar_view.dart';
 
 class StartMission extends StatefulWidget {
@@ -18,23 +22,27 @@ class StartMission extends StatefulWidget {
 class _StartMissionState extends State<StartMission> {
   String? duration = "un jour";
 
+  DateTime? _day;
+
   bool enEquipe = false;
   bool enDeplacement = false;
   late TextEditingController nomVehicule;
   late TextEditingController person;
   late TextEditingController indexVehicule;
   late TextEditingController tarif;
-  late TextEditingController nombrejour;
+  late TextEditingController site;
   int bonCarburant = 1;
+  int jourdep = 1;
 
   List<String> equipes = [];
+  List<String> sites = [];
 
   @override
   void initState() {
     nomVehicule = TextEditingController();
     indexVehicule = TextEditingController();
     tarif = TextEditingController();
-    nombrejour = TextEditingController();
+    site = TextEditingController();
     person = TextEditingController();
     super.initState();
   }
@@ -44,7 +52,7 @@ class _StartMissionState extends State<StartMission> {
     nomVehicule.dispose();
     indexVehicule.dispose();
     tarif.dispose();
-    nombrejour.dispose();
+    site.dispose();
     person.dispose();
     super.dispose();
   }
@@ -79,11 +87,25 @@ class _StartMissionState extends State<StartMission> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const SubTitleComponent(
-                  title: "Date de depart :",
+                Row(
+                  children: [
+                    const SubTitleComponent(
+                      title: "Date de depart :",
+                    ),
+                    Text(
+                      _day == null ? "" : _day!.toString(),
+                    ),
+                  ],
                 ),
                 InkWell(
-                  onTap: () {},
+                  onTap: () async {
+                    _day = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2023),
+                      lastDate: DateTime(2027),
+                    );
+                  },
                   child: const Icon(
                     Icons.calendar_month_outlined,
                   ),
@@ -101,18 +123,19 @@ class _StartMissionState extends State<StartMission> {
                 ),
                 Container(
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
+                    border: Border.all(color: Colors.black26),
                   ),
                   child: DropdownButtonHideUnderline(
                     child: Padding(
                       padding: const EdgeInsets.all(padding10 / 2),
                       child: DropdownButton<String>(
+                        borderRadius: BorderRadius.circular(10),
                         alignment: Alignment.topCenter,
                         value: duration,
                         isDense: true,
-                        dropdownColor: Colors.grey.withOpacity(1),
+                        dropdownColor: Colors.grey.shade900,
                         itemHeight: 50,
-                        elevation: 10,
+                        elevation: 0,
                         onChanged: (value) {
                           setState(() {
                             duration = value;
@@ -193,12 +216,14 @@ class _StartMissionState extends State<StartMission> {
                                 content: Column(
                                   children: [
                                     const Text(
-                                        "Ajouter vos collegues en mission"),
+                                      "Vos collegues en mission",
+                                    ),
                                     const SizedBox(
                                       height: padding10,
                                     ),
                                     CupertinoTextField(
-                                      controller: TextEditingController(),
+                                      controller: person,
+                                      autocorrect: false,
                                       cursorColor: Colors.white,
                                       style: const TextStyle(
                                         color: Colors.white,
@@ -214,15 +239,20 @@ class _StartMissionState extends State<StartMission> {
                                       Get.back();
                                     },
                                     child: const Text(
-                                      "cancel",
+                                      "Retour",
                                     ),
                                   ),
                                   TextButton(
                                     onPressed: () {
-                                      equipes.add(person.text);
+                                      setState(() {
+                                        if (person.text.isNotEmpty) {
+                                          equipes.add(person.text);
+                                          person.clear();
+                                        }
+                                      });
                                     },
                                     child: const Text(
-                                      "done",
+                                      "Ajouter",
                                     ),
                                   ),
                                 ],
@@ -234,6 +264,60 @@ class _StartMissionState extends State<StartMission> {
                     ),
                   )
                 : Container(),
+            ...List.generate(equipes.length, (index) {
+              String nom = equipes[index];
+              return enEquipe
+                  ? ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      title: Text(
+                        nom,
+                        style:
+                            Theme.of(context).textTheme.labelLarge!.copyWith(),
+                      ),
+                      trailing: InkWell(
+                        borderRadius: BorderRadius.circular(10),
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return CupertinoAlertDialog(
+                                  content: Text(
+                                    "voulez vous enlever $nom depuis cette mission",
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        Get.back();
+                                      },
+                                      child: const Text("Retour"),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          equipes.removeAt(index);
+                                        });
+
+                                        Get.back();
+                                      },
+                                      child: const Text("Confirm"),
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.remove_circle_outline,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container();
+            }),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -265,13 +349,21 @@ class _StartMissionState extends State<StartMission> {
             enDeplacement
                 ? Column(
                     children: [
-                      const SizedBox(
-                        height: padding10 / 2,
-                      ),
-                      NomFormField(
-                        labelText: "nombre de jour",
-                        hintText: "nombre de jour en deplacement",
-                        controller: tarif,
+                      AddandRemouveComponent(
+                        title: "jour en deplacement:",
+                        bonCarburant: jourdep,
+                        increase: () {
+                          setState(() {
+                            jourdep++;
+                          });
+                        },
+                        decrease: () {
+                          setState(() {
+                            if (jourdep > 0) {
+                              jourdep--;
+                            }
+                          });
+                        },
                       ),
                       const SizedBox(
                         height: padding10,
@@ -279,7 +371,7 @@ class _StartMissionState extends State<StartMission> {
                       NomFormField(
                         labelText: "frais de deplacement",
                         hintText: "tarif de deplacement en dinar",
-                        controller: nombrejour,
+                        controller: tarif,
                       ),
                     ],
                   )
@@ -291,11 +383,88 @@ class _StartMissionState extends State<StartMission> {
                   title: "Codes sites :",
                 ),
                 TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        barrierColor: Colors.black26,
+                        builder: (context) {
+                          return CupertinoAlertDialog(
+                            content: Column(
+                              children: [
+                                const Text(
+                                  "Ajouter les code sites ",
+                                ),
+                                const SizedBox(
+                                  height: padding10,
+                                ),
+                                CupertinoTextField(
+                                  controller: site,
+                                  autocorrect: false,
+                                  cursorColor: Colors.white,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                  decoration: const BoxDecoration(
+                                      color: Colors.black38),
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: const Text(
+                                  "Retour",
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (site.text.isNotEmpty) {
+                                      sites.add(site.text);
+                                      site.clear();
+                                    }
+                                  });
+                                },
+                                child: const Text(
+                                  "Ajouter",
+                                ),
+                              ),
+                            ],
+                          );
+                        });
+                  },
                   icon: const Icon(Icons.add),
                   label: const Text("Ajouter"),
                 )
               ],
+            ),
+            Wrap(
+              spacing: 15.0,
+              direction: Axis.horizontal,
+              children: List<Widget>.generate(
+                sites.length,
+                (int index) {
+                  final site = sites[index];
+                  return Chip(
+                    label: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          sites.removeAt(index);
+                        });
+                      },
+                      child: Text(
+                        site,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                    ),
+                    autofocus: true,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10.0),
+                  );
+                },
+              ).toList(),
             ),
             const SizedBox(
               height: padding10 / 2,
@@ -314,7 +483,7 @@ class _StartMissionState extends State<StartMission> {
             ),
             NomFormField(
               labelText: "Nom de Vehicule",
-              hintText: "Tata ....",
+              hintText: "Tata 240-Tunis-98xx",
               controller: TextEditingController(),
             ),
             const SizedBox(
@@ -328,52 +497,21 @@ class _StartMissionState extends State<StartMission> {
             const SizedBox(
               height: padding10 / 2,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SubTitleComponent(
-                  title: "Bon de carburants :",
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(
-                          Colors.grey.shade800,
-                        ),
-                      ),
-                      child: const Icon(
-                        Icons.add,
-                        size: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: padding10 / 2,
-                    ),
-                    Text(
-                      "0",
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                    const SizedBox(
-                      width: padding10 / 2,
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(
-                          Colors.grey.shade800,
-                        ),
-                      ),
-                      child: const Icon(
-                        Icons.remove_outlined,
-                        size: 20,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+            AddandRemouveComponent(
+              title: "Bon de carburants :",
+              bonCarburant: bonCarburant,
+              increase: () {
+                setState(() {
+                  bonCarburant++;
+                });
+              },
+              decrease: () {
+                setState(() {
+                  if (bonCarburant > 1) {
+                    bonCarburant--;
+                  }
+                });
+              },
             ),
             const SizedBox(
               height: padding10,
@@ -381,6 +519,72 @@ class _StartMissionState extends State<StartMission> {
           ],
         ),
       ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class AddandRemouveComponent extends StatelessWidget {
+  const AddandRemouveComponent({
+    Key? key,
+    required this.title,
+    this.increase,
+    this.decrease,
+    required this.bonCarburant,
+  }) : super(key: key);
+  final String title;
+  final int bonCarburant;
+  final VoidCallback? increase;
+  final VoidCallback? decrease;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SubTitleComponent(
+          title: title,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(
+              onPressed: decrease,
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(
+                  Colors.grey.shade800,
+                ),
+              ),
+              child: const Icon(
+                Icons.remove_outlined,
+                size: 20,
+              ),
+            ),
+            const SizedBox(
+              width: padding10 / 2,
+            ),
+            Text(
+              bonCarburant.toString(),
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const SizedBox(
+              width: padding10 / 2,
+            ),
+            TextButton(
+              onPressed: increase,
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(
+                  Colors.grey.shade800,
+                ),
+              ),
+              child: const Icon(
+                Icons.add,
+                size: 20,
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
